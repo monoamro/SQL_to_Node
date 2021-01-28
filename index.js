ba5571342d03d17383f99ead6022d273f42e8a11
Devis <22038252+DevisWeb@users.noreply.github.com>
2021-01-28 22:12:49 +0100

    Add 1st endpoint for posts + redirect | Add get request to homepage
    * index.js (root):
      - import & use routes
      - add get request to homepage (../posts will then be redirected to ../)
    
    * controllers/posts.js:
      - Add first endpoint with handler for '../posts'
        with redirect to homepage '../'
        -> test redirect and endpoint: http://localhost:3000/posts
      - remove obsolete code
    
    * routers/posts.js
      - Add router.get for ../posts/
    
    * routers/posts.js & routers/users.js:
      - Add comment-list for endpoints that will be required

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// Create entry point
 const express = require('express');
 const app = express();
 const port = 3000;
-
+// import routes
 const postsRoutes = require('./routes/posts');
 
-app.use("/posts", postsRoutes);
+// use postsRoutes
+app.use('/posts', postsRoutes);
+
+// get request to homepage
+app.get('/', (req, res) => res.send('main page - will show all posts'));
+
+// Starting server
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+// test our first endpoint:
+// http://localhost:3000/posts --> should be redirected to http://localhost:3000/
