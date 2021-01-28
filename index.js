158b8ee1c93224c2734ec86e7fbcd919c28d7e36
Devis <22038252+DevisWeb@users.noreply.github.com>
2021-01-28 16:21:43 +0100

    Define first endpoint on '/' for testing

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+// Create entry point
 const express = require('express');
 const app = express();
 const port = 3000;
 
-const postsRoutes = require('./routes/posts');
+// const postsRoutes = require('./routes/posts');
+// app.use('/posts', postsRoutes);
 
-app.use("/posts", postsRoutes);
+// Defining an endpoint and handler for the endpoint
+// app.get requires two params: path, callback (middleware)
+app.get('/', (req, res) => res.send('YOU have accessed / on my api'));
+
+// Starting server
+app.listen(port, () => console.log(`Server listening on port ${port}`));
