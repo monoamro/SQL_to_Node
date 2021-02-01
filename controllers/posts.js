ebef09ce5c5d38e988b839907ee54350e335f428
Montaser Amro <mono.amro@gmail.com>
2021-02-01 17:35:41 +0200

    reorganizes the routes inside postsRoutes

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,11 @@
 const postsController = {
   logRequest: (req, res, next) => {
-    res.end('There was a request made on /posts');
+    console.log("There was a request made on /posts");
+    next();
   },
   getAll: (req, res) => {
     // sql work related stuff
+    res.send("here are all the posts")
     // send back the data as a json
   },
 };
