21dd95222ec24ad56e2afc5b0818e463572292af
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-05 10:14:03 +0100

    Small corrections after merging feature branches

--- functions.js
+++ functions.js
@@ -1,15 +1,14 @@
 const functions = {
-    verifyQuery: (orderby, sort) => {
-        const okColumns = ['rating', 'topicId'];
-        const okSorts = ['desc', 'asc'];
-        if(okColumns.includes(orderby) && okSorts.includes(sort)) {
-            return;
-        } else {
-            const responce = {status: 400, message: "wrong sort type"}
-            throw responce;
-        }
+  verifyQuery: (orderby, sort) => {
+    const okColumns = ['rating', 'topicid'];
+    const okSorts = ['desc', 'asc'];
+    if (okColumns.includes(orderby) && okSorts.includes(sort)) {
+      return;
+    } else {
+      const response = { status: 400, message: 'wrong sort type' };
+      throw response;
     }
-
-}
+  },
+};
 
 module.exports = functions;
